15077beeefc133581c3be7943e189abc7fbed154
Macarthur-yaw <97068073+Macarthur-yaw@users.noreply.github.com>
2022-10-15 07:27:36 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+function openForm() {
+  document.getElementById("sign").style.display = "block"; //display elements hidden
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none"; //hides elements shown
+}
+function opensubForm() {
+  document.getElementById("up").style.display="block";
+  document.getElementById("sign").style.display = "none";
+}
